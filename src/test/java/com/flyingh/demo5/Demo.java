164d84a2dc771d29314c58bc3256d6c25585134b
flycoding <flycoding@yeah.net>
2013-05-12 00:34:44 +0800

    test print 'M'.

---
+++ src/test/java/com/flyingh/demo5/Demo.java
@@ -0,0 +1,43 @@
+package com.flyingh.demo5;
+
+import org.junit.Test;
+
+public class Demo {
+
+	private static final int M = 3;
+	private static final int N = 9;
+
+	@Test
+	public void test() {
+		int[][] array = new int[M][N];
+		int row = M;
+		boolean isUp = true;
+		for (int i = 0; i < N; i++) {
+			if (isUp) {
+				array[--row][i] = i + 1;
+				if (row == 0) {
+					isUp = false;
+				}
+			} else {
+				array[++row][i] = i + 1;
+				if (row == M - 1) {
+					isUp = true;
+				}
+			}
+		}
+		print(array);
+	}
+
+	private void print(int[][] array) {
+		for (int[] is : array) {
+			for (int i : is) {
+				if (i == 0) {
+					System.out.print(" ");
+				} else {
+					System.out.print(i);
+				}
+			}
+			System.out.println();
+		}
+	}
+}
